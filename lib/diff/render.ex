fd7b5220dd1abcbb624abb2cc71cf1f1a2a409bb
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2019-09-13 18:02:38 +0200

    very basic

---
+++ lib/diff/render.ex
@@ -0,0 +1,64 @@
+defmodule Diff.Render do
+
+  def line_number(ln) when is_nil(ln), do: ""
+  def line_number(ln), do: to_string(ln)
+
+  def line_id(patch, line) do
+    hash = :erlang.phash2({patch.from, patch.to})
+
+    ln = line.from_line_number || line.to_line_number
+
+    [to_string(hash), to_string(ln)]
+  end
+
+  def line_type(line), do: to_string(line.type)
+
+  def diff_to_html(diff) do
+    [
+      "<div class=\"ghd-container\">",
+      for patch <- diff do
+        [
+          "<div class=\"ghd-file\">",
+          "<div class=\"ghd-file-header\">",
+          patch.from,
+          "</div>",
+          "<div class=\"ghd-diff\">",
+          "<table class=\"ghd-diff\">",
+          for chunk <- patch.chunks do
+            [
+              "<tr class=\"ghd-chunk-header\">",
+              "<td class=\"ghd-line-number\"><div>&nbsp;</div></td>",
+              "<td class=\"ghd-text-internal\">",
+              chunk.header,
+              "</td>",
+              "</tr>",
+              for line <- chunk.lines do
+                [
+                  "<tr id=\"", line_id(patch, line), "\" class=\"ghd-line ghd-line-type-", line_type(line) ,"\">",
+                  "<td class=\"ghd-line-number\">",
+                  "<div class=\"ghd-line-number-from\">",
+                  line_number(line.from_line_number),
+                  "</div>",
+                  "<div class=\"ghd-line-number-to\">",
+                  line_number(line.to_line_number),
+                  "</div>",
+                  "</td>",
+                  "<td class=\"ghd-text\">",
+                  "<div class=\"ghd-text-internal\">",
+                  Phoenix.HTML.Engine.encode_to_iodata!(line.text),
+                  "</div>",
+                  "</td>",
+                  "</tr>"
+                ]
+              end
+            ]
+          end,
+          "</table>",
+          "</div>",
+          "</div>"
+        ]
+      end,
+      "</div>"
+    ]
+  end
+end
