416eade5189afa4f51330822c7474dea538bd78a
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2019-11-06 12:20:29 +0100

    Nicer headers and some cleanup

--- lib/diff/render.ex
+++ lib/diff/render.ex
@@ -1,5 +1,35 @@
 defmodule Diff.Render do
   import Phoenix.HTML
+  def file_header(patch, status) do
+    from = patch.from
+    to = patch.to
+
+    case status do
+      "changed" -> from
+      "renamed" -> "#{from} -> #{to}"
+      "removed" -> from
+      "added" -> to
+    end
+  end
+
+  def patch_status(patch) do
+    from = patch.from
+    to = patch.to
+
+    if from && to do
+      if from == to do
+        "changed"
+      else
+        "renamed"
+      end
+    else
+      if from do
+        "removed"
+      else
+        "added"
+      end
+    end
+  end
 
   def line_number(ln) when is_nil(ln), do: ""
   def line_number(ln), do: to_string(ln)
@@ -14,20 +44,30 @@ defmodule Diff.Render do
 
   def line_type(line), do: to_string(line.type)
 
+
   def diff_to_html(diff) do
     html = ~E"""
     <div class="ghd-container">
       <%= for patch <- diff do %>
+        <% status = patch_status(patch) %>
         <div class="ghd-file">
           <div class="ghd-file-header">
-            <%= patch.from %>
+            <span class="ghd-file-status ghd-file-status-<%= status %>">
+              <%= status %>
+            </span>
+            <%= file_header(patch, status) %>
           </div>
           <div class="ghd-diff">
             <table class="ghd-diff">
               <%= for chunk <- patch.chunks do %>
                 <tr class="ghd-chunk-header">
-                  <td class="ghd-line-number"></td>
-                  <td class="ghd-text-internal"><%= chunk.header %></td>
+                  <td class="ghd-line-number">
+                    <div class="ghd-line-number-from">&nbsp;</div>
+                    <div class="ghd-line-number-to"></div>
+                  </td>
+                  <td class="ghd-text">
+                    <div class="ghd-text-internal"><%= chunk.header %></div>
+                  </td>
                 </tr>
                 <%= for line <- chunk.lines do %>
                   <tr id="<%= line_id(patch, line) %>" class="ghd-line ghd-line-type-<%= line_type(line) %>">
