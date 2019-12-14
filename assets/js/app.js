2f57fe66550fc766190616310dd3379947398a63
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2019-12-14 14:42:40 +0100

    Remove comment

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,17 +11,11 @@ import css from "../css/app.css"
 //
 import "phoenix_html"
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
-
 import { Socket } from 'phoenix'
 import { LiveSocket } from "phoenix_live_view"
 
 let liveSocket = new LiveSocket("/live", Socket, {})
 liveSocket.connect()
-
 
 /*
 Make it possible to click line numbers to update the address bar to a
