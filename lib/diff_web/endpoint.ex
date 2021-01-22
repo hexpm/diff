f210175c4a7727bb27e2c688c3d423d6e578e32b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-01-22 12:21:48 +0000

    gzip assets

--- lib/diff_web/endpoint.ex
+++ lib/diff_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule DiffWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :diff,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
