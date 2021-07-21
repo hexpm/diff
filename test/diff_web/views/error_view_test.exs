5851c948d52a453eb7247130928ab6df34392342
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-07-21 18:37:10 +0200

    Fix error pages

--- test/diff_web/views/error_view_test.exs
+++ test/diff_web/views/error_view_test.exs
@@ -5,10 +5,10 @@ defmodule DiffWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(DiffWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(DiffWeb.ErrorView, "404.html", []) =~ "Page not found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(DiffWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(DiffWeb.ErrorView, "500.html", []) =~ "Something went wrong"
   end
 end
