342ae8abd07af2e87f4432a8bcbe20d64c572d3c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2024-12-15 23:38:14 +0100

    Replace rollbar with sentry

--- lib/diff_web/endpoint.ex
+++ lib/diff_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule DiffWeb.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :diff
 
   socket "/live", Phoenix.LiveView.Socket
@@ -32,6 +33,7 @@ defmodule DiffWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
 
