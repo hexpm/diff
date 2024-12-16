50da03d03de0f1c076d6d0e6967f085c6f4300ea
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2024-12-16 01:09:38 +0100

    Update webpack deps

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :diff, DiffWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
+      "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
